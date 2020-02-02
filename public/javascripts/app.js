3ba69d9822b41fe377c5fa4d671acde79cd7047b
blank <blankycan@gmail.com>
2020-02-02 11:44:22 +0100

    Separated out the code for the video player into a mixin.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,15 @@
+let webSocketServer = "wss://<insert.address.of.websocket.server>";
+
 var app = new Vue({
   el: '#app',
+  mixins: [ videoPlayer ],
   data: {
     socket: null,
     timesync: null,
-    player: null,
     videoSearch: "https://www.youtube.com/watch?v=S-8U4lSEq8A",
-    playing: false,
-    lastVideoTime: 0,
-    lastTimestamp: 0,
-    playSync: null,
-    syncCount: 0
   },
   created() {
+    // Setup the TimeSync
     this.timesync = timesync.create({
       server: '/timesync',
       interval: 10000
@@ -21,9 +19,8 @@ var app = new Vue({
       console.log("Changed offset:", offset, "ms");
     });
 
-    let localhost = "ws://192.168.1.102:3031";
-    let external = "wss://2g4ws.blankycan.com";
-    this.socket = new WebSocket((window.location.host.includes(':3030')) ? localhost : external);
+    // Setup the WebSocket towards our video server
+    this.socket = new WebSocket(webSocketServer);
 
     this.socket.addEventListener('open', function(event) {
       let msg = {
@@ -36,6 +33,9 @@ var app = new Vue({
     this.socket.addEventListener('message', this.onMessage);
   },
   methods: {
+    /**
+     * Handle message received from the WebSocket.
+     */
     onMessage: function(inMessage) {
       console.log("onMessage:", inMessage);
       let data = {};
@@ -46,28 +46,26 @@ var app = new Vue({
         console.log("Failed to parse message.");
       }
 
+      // Handle video search event
       if(data.type === "search") {
         this.doSearch(data.data);
       }
+      // Handle a Playback event
       else if(data.type === "playback") {
         this.lastVideoTime = data.time;
         this.lastTimestamp = data.timestamp;
+
+        // Play
         if(data.action === "play") {
           this.doPlay(this.calculateVideoTime());
           this.setupPlaySync();
         }
+
+        // Pause
         else if(data.action === "pause") {
           this.doPause(data.time);
         }
       }
-    },
-    calculateVideoTime: function() {
-      let now = (new Date(this.timesync.now())).getTime();
-      let diff = (now - this.lastTimestamp) / 1000.0
-      console.log("Timediff is:", diff, 'seconds');
-      this.lastVideoTime = this.lastVideoTime + diff;
-      this.lastTimestamp = now;
-      return this.lastVideoTime;
     },
     /**
      * Do a very crude modification to given YouTube links to make them work
@@ -91,121 +89,6 @@ var app = new Vue({
         data: search
       }
       this.socket.send(JSON.stringify(msg));
-    },
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    onYouTubeIFrameAPIReady: function() {
-      console.log("IN HERE");
-      this.player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'FQRsakvoe8w',
-        playerVars: { 'autoplay': 0 },
-        events: {
-          'onReady': this.onPlayerReady,
-          'onStateChange': this.onPlayerStateChange
-        }
-      });
-    },
-    // 4. The API will call this function when the video player is ready.
-    onPlayerReady: function(event) {
-      console.log("onPLayerReady");
-      this.doStop();
-    },
-    onPlayerStateChange: function(event) {
-      console.log(event);
-      if(event.data == YT.PlayerState.ENDED) {
-        console.log("ENDED");
-      }
-      else if(event.data == YT.PlayerState.PLAYING) {
-        console.log("PLAYING");
-        if(!this.playing) {
-          this.lastVideoTime = this.player.getCurrentTime();
-          this.lastTimestamp = (new Date(this.timesync.now())).getTime();
-          let msg = {
-            type: 'playback',
-            action: 'play',
-            time: this.lastVideoTime,
-            timestamp: this.lastTimestamp
-          }
-          this.socket.send(JSON.stringify(msg));
-          this.setupPlaySync();
-        }
-        this.playing = true;
-      }
-      else if(event.data == YT.PlayerState.PAUSED) {
-        console.log("PAUSED");
-        if(this.playing) {
-          let msg = {
-            type: 'playback',
-            action: 'pause',
-            time: this.player.getCurrentTime(),
-            timestamp: (new Date(this.timesync.now())).getTime()
-          }
-          this.socket.send(JSON.stringify(msg));
-        }
-        this.playing = false;
-      }
-      else if(event.data == YT.PlayerState.BUFFERING) {
-        console.log("BUFFERING");
-        if(!this.playing) {
-          let msg = {
-            type: 'playback',
-            action: 'play',
-            time: this.player.getCurrentTime(),
-            timestamp: (new Date(this.timesync.now())).getTime()
-          }
-          this.socket.send(JSON.stringify(msg));
-        }
-      }
-      else if(event.data == YT.PlayerState.CUED) {
-        console.log("CUED");
-      }
-      else {
-        console.log("Other event change", event);
-      }
-    },
-    setupPlaySync: function() {
-      if(this.playSync) {
-        clearInterval(this.playSync);
-      }
-      this.syncCount = 5;
-      this.playSync = setInterval(this.syncPlayTime, 200);
-    },
-    syncPlayTime: function() {
-      console.log("syncPlayTime", this.syncCount, this.playSync);
-      if(this.playing && this.syncCount-- > 0){
-        this.player.seekTo(this.calculateVideoTime());
-      }
-      else {
-        if(this.playSync) {
-          clearInterval(this.playSync);
-        }
-      }
-    },
-    doSearch: function(inUrl) {
-      console.log("doSearch:", inUrl);
-      this.player.loadVideoByUrl(inUrl, 0)
-    },
-    doPlay: function(inTime) {
-      console.log("PlayVideo", inTime);
-      this.playing = true;
-      this.player.playVideo();
-      this.player.seekTo(inTime, true);
-    },
-    doPause: function(inTime) {
-      console.log("StopVideo", inTime);
-      this.playing = false;
-      this.player.pauseVideo();
-      this.player.seekTo(inTime, true);
-    },
-    doStop: function(inTime) {
-      console.log("StopVideo", inTime);
-      this.playing = false;
-      this.player.stopVideo();
     }
-  },
-  mounted() {
-    console.log("MOUNTED");
   }
 });
---
+++ public/javascripts/videoPlayer.js
@@ -0,0 +1,181 @@
+var videoPlayer = {
+  el: '#app',
+  data: function() {
+    return {
+      player: null,
+      playing: false,
+      lastVideoTime: 0,
+      lastTimestamp: 0,
+      playSync: null,
+      syncCount: 0
+    }
+  },
+  methods: {
+    /**
+     * Calculate what time the video should be on now.
+     * @return The time that the video should be on.
+     */
+    calculateVideoTime: function() {
+      let now = (new Date(this.timesync.now())).getTime();
+      let diff = (now - this.lastTimestamp) / 1000.0
+      console.log("Timediff is:", diff, 'seconds');
+      this.lastVideoTime = this.lastVideoTime + diff;
+      this.lastTimestamp = now;
+      return this.lastVideoTime;
+    },
+    /**
+     * When the YouTube IFrame API is ready, setup the YouTube player in
+     * the iframe and connect some events.
+     */
+    onYouTubeIFrameAPIReady: function() {
+      console.log("IN HERE");
+      this.player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'FQRsakvoe8w',
+        playerVars: { 'autoplay': 0 },
+        events: {
+          'onReady': this.onPlayerReady,
+          'onStateChange': this.onPlayerStateChange
+        }
+      });
+    },
+    /**
+     * Callback when the Video player is ready after loading a video.
+     */
+    onPlayerReady: function(event) {
+      console.log("onPlayerReady");
+      this.doStop();
+    },
+    /**
+     * Callback when the Video player have switched state.
+     * This is how we detect that the user have started playing or
+     * paused the video, and need to notify all other users through
+     * the web socket.
+     * @param event The YouTube player state change event.
+     */
+    onPlayerStateChange: function(event) {
+      console.log(event);
+      // Video has Ended
+      if(event.data == YT.PlayerState.ENDED) {
+        console.log("ENDED");
+      }
+      // Video has started Playing
+      else if(event.data == YT.PlayerState.PLAYING) {
+        console.log("PLAYING");
+        if(!this.playing) {
+          this.lastVideoTime = this.player.getCurrentTime();
+          this.lastTimestamp = (new Date(this.timesync.now())).getTime();
+          let msg = {
+            type: 'playback',
+            action: 'play',
+            time: this.lastVideoTime,
+            timestamp: this.lastTimestamp
+          }
+          this.socket.send(JSON.stringify(msg));
+          this.setupPlaySync();
+        }
+        this.playing = true;
+      }
+      // Video has been Paused
+      else if(event.data == YT.PlayerState.PAUSED) {
+        console.log("PAUSED");
+        if(this.playing) {
+          let msg = {
+            type: 'playback',
+            action: 'pause',
+            time: this.player.getCurrentTime(),
+            timestamp: (new Date(this.timesync.now())).getTime()
+          }
+          this.socket.send(JSON.stringify(msg));
+        }
+        this.playing = false;
+      }
+      // Video is Buffering
+      else if(event.data == YT.PlayerState.BUFFERING) {
+        console.log("BUFFERING");
+        if(!this.playing) {
+          let msg = {
+            type: 'playback',
+            action: 'play',
+            time: this.player.getCurrentTime(),
+            timestamp: (new Date(this.timesync.now())).getTime()
+          }
+          this.socket.send(JSON.stringify(msg));
+        }
+      }
+      // Video has been Cued
+      else if(event.data == YT.PlayerState.CUED) {
+        console.log("CUED");
+      }
+      // Unknown
+      else {
+        console.log("Other event change", event);
+      }
+    },
+    /**
+     * Trying to synchronize the YouTube players, so all clients are
+     * playing at the same time in the video.
+     * Try setting an interval at 200ms where we seek the correct time
+     * in the video.
+     */
+    setupPlaySync: function() {
+      if(this.playSync) {
+        clearInterval(this.playSync);
+      }
+      this.syncCount = 5;
+      this.playSync = setInterval(this.syncPlayTime, 200);
+    },
+    /**
+     * Perform the synchronization of the YouTube player, by calculating
+     * what time it should be on and seeking towards that time.
+     */
+    syncPlayTime: function() {
+      console.log("syncPlayTime", this.syncCount, this.playSync);
+      if(this.playing && this.syncCount-- > 0){
+        this.player.seekTo(this.calculateVideoTime());
+      }
+      else {
+        if(this.playSync) {
+          clearInterval(this.playSync);
+        }
+      }
+    },
+    /**
+     * Load a YouTube video by url.
+     * @param inUrl Url to load in the player.
+     */
+    doSearch: function(inUrl) {
+      console.log("doSearch:", inUrl);
+      this.player.loadVideoByUrl(inUrl, 0)
+    },
+    /**
+     * Play the YouTube player, and seek it to a certain time.
+     * @param inTime Time to seek the video to.
+     */
+    doPlay: function(inTime) {
+      console.log("PlayVideo", inTime);
+      this.playing = true;
+      this.player.playVideo();
+      this.player.seekTo(inTime, true);
+    },
+    /**
+     * Pause the YouTube player, and seek it to a certain time.
+     * @param inTime Time to seek the video to.
+     */
+    doPause: function(inTime) {
+      console.log("PauseVideo", inTime);
+      this.playing = false;
+      this.player.pauseVideo();
+      this.player.seekTo(inTime, true);
+    },
+    /**
+     * Stop the YouTube player.
+     */
+    doStop: function() {
+      console.log("StopVideo");
+      this.playing = false;
+      this.player.stopVideo();
+    }
+  }
+}
